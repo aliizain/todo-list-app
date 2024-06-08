ecdaa981da6594186cbba56b8585af1e4fb874ce
Ali Zain <alizainrajani2@gmail.com>
2024-06-08 23:43:12 +0500

    Implement TaskListScreen UI

--- app/src/main/java/com/example/todolistapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/todolistapp/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.example.todolistapp.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
@@ -9,3 +10,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val backgroundColorLight = Color(0xFFF1F1F1)
+val backgroundColorDark = Color(0xFFE4E4E4)
+
+val backgroundColor = Brush.verticalGradient(0.5f to backgroundColorLight, 1f to backgroundColorDark)
+
+val textColor = Color(0xFF333333)
+val buttonColor = Color(0xFFD5A005)
+val checkedColor = Color(0xFF18C034)
